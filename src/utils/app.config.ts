060da1b9fa6ed89215e4e0a5e3150f8797e5e5c7
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-10-15 16:56:23 +0700

    Change port

--- src/utils/app.config.ts
+++ src/utils/app.config.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 export default class Config {
-  static PORT = parseInt(process.env.PORT || '3000');
+  static PORT = parseInt(process.env.PORT || '8080');
   static DATABASE_PORT = parseInt(process.env.DATABASE_PORT || '3306');
   static DATABASE_HOST = process.env.DATABASE_HOST || '127.0.0.1';
   static DATABASE_USERNAME = process.env.DATABASE_USERNAME || 'root';
